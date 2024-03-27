3fcdc084dff154754c53419b43e16511b9faaa17
Zihao Wang <zi.wang@ostfalia.de>
2024-03-27 22:36:17 +0100

    Anlage und Anzeige von Kunden- und Produktdaten

---
+++ src/main/java/de/pdbm/starter/business/messages/boundary/table/CustomerTable.java
@@ -0,0 +1,25 @@
+package de.pdbm.starter.business.messages.boundary.table;
+
+import de.pdbm.starter.business.messages.control.CustomerService;
+import de.pdbm.starter.business.messages.entity.Customer;
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Named
+@ViewScoped
+public class CustomerTable implements Serializable {
+    @Inject
+    CustomerService customerService;
+
+    public List<Customer> getCustomers() {
+        return customerService.findAll();
+    }
+
+    public String navigateToHomePage() {
+        return "homePage.xhtml";
+    }
+}
