d50696ef7bfafbf13ca7ae4107f05711727c34ef
Zijian Ying <z.ying@ostfalia.de>
2024-04-05 22:05:08 +0200

    neue Eigenschaft einfuegen und unnoetige Attribute loeschen

--- src/main/java/de/pdbm/starter/business/messages/entity/Customer.java
+++ src/main/java/de/pdbm/starter/business/messages/entity/Customer.java
@@ -10,41 +10,60 @@ public class Customer {
     @Column(name = "customer_id")
     private Integer id;
 
-    @Column(name = "customer_firstname")
-    private String firstname;
-
-    @Column(name = "customer_lastname")
-    private String lastname;
-
-    private String address;
-
-    private String phone;
+    private String city;
 
     private String email;
+    @Column(name = "first_name")
+    private String firstname;
 
-    public Customer(Integer id, String firstname, String lastname, String address, String phone, String email) {
+    @Column(name = "last_name")
+    private String lastname;
+    private String phone;
+//    private String address; loesch unnoetig
+
+
+
+
+    private String state;
+    private String street;
+    @Column(name = "zip_code")
+    private String zipcode;
+
+//    public Customer(Integer id, String firstname, String lastname, String address, String phone, String email) {
+//        this.id = id;
+//        this.firstname = firstname;
+//        this.lastname = lastname;
+//        this.address = address;
+//        this.phone = phone;
+//        this.email = email;
+//    }
+
+    public Customer(Integer id, String city, String email, String firstname, String lastname, String phone, String state, String street, String zipcode) {
         this.id = id;
+        this.city = city;
+        this.email = email;
         this.firstname = firstname;
         this.lastname = lastname;
-        this.address = address;
         this.phone = phone;
-        this.email = email;
+        this.state = state;
+        this.street = street;
+        this.zipcode = zipcode;
     }
 
     public Customer() {
     }
 
-    @Override
-    public String toString() {
-        return "Kunde{" +
-                "id=" + id +
-                ", vorname='" + firstname + '\'' +
-                ", nachname='" + lastname + '\'' +
-                ", adresse='" + address + '\'' +
-                ", telefonnummer='" + phone + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
+//    @Override
+//    public String toString() {
+//        return "Kunde{" +
+//                "id=" + id +
+//                ", vorname='" + firstname + '\'' +
+//                ", nachname='" + lastname + '\'' +
+//                ", adresse='" + address + '\'' +
+//                ", telefonnummer='" + phone + '\'' +
+//                ", email='" + email + '\'' +
+//                '}';
+//    }
 
     public Integer getId() {
         return id;
@@ -70,13 +89,13 @@ public class Customer {
         this.lastname = lastname;
     }
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
+//    public String getAddress() {
+//        return address;
+//    }
+//
+//    public void setAddress(String address) {
+//        this.address = address;
+//    } adress unnoetig
 
     public String getPhone() {
         return phone;
@@ -92,5 +111,37 @@ public class Customer {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
     }
 }
