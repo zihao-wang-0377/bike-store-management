3a63440e515d23afbeb97875c508f857d7125de9
id862316 <fel.bischoff@ostfalia.de>
2024-04-05 11:46:00 +0200

    Kommentare im Code

--- src/main/java/de/pdbm/starter/business/messages/control/OrderService.java
+++ src/main/java/de/pdbm/starter/business/messages/control/OrderService.java
@@ -17,19 +17,25 @@ public class OrderService implements Serializable {
     EntityManager em;
 
     public void save(Order order){
+        // Persistiere (speichere) die übergebene Bestellung in der Datenbank
         em.persist(order);
     }
 
     public List<Order> findAll(){
+        // Erstelle eine Abfrage, um alle Bestellungen und ihre Kunden aus der Datenbank abzurufen
         TypedQuery<Order> query = em.createQuery(
                 "select o from Order o join fetch o.customer", Order.class
         );
+
+        // Führe die Abfrage aus und gib die Ergebnisliste zurück
         return query.getResultList();
     }
 
     public Order findById(Integer id){
+        // Suche eine Bestellung in der Datenbank anhand ihrer ID
         return em.find(Order.class, id);
     }
+
 
     public void delete(Order order) {
         em.remove(order);
