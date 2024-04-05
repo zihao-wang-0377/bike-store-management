254222c500bf623299396bff439b182a26c890bf
Zijian Ying <z.ying@ostfalia.de>
2024-04-05 23:07:55 +0200

    kleine korrektur

--- src/main/java/de/pdbm/starter/business/messages/entity/Product.java
+++ src/main/java/de/pdbm/starter/business/messages/entity/Product.java
@@ -20,6 +20,12 @@ public class Product {
 private Integer modelYear;
     @Column(name = "product_name")
     private String name;
+    @ManyToOne
+    @JoinColumn(name = "brand_id")
+    private Brand brand;
+    @ManyToOne
+@JoinColumn(name = "category_id")
+    private Category category;
 
 //    private String description;
 
@@ -33,13 +39,16 @@ private Integer modelYear;
     public Product() {
     }
 
-    public Product(Integer id, BigDecimal price, Integer modelYear, String name) {
+    public Product(Integer id, BigDecimal price, Integer modelYear, String name, Brand brand, Category category) {
         this.id = id;
         this.price = price;
         this.modelYear = modelYear;
         this.name = name;
+        this.brand = brand;
+        this.category = category;
     }
-//    @Override
+
+    //    @Override
 //    public String toString() {
 //        return "Produkt{" +
 //                "id=" + id +
@@ -88,5 +97,21 @@ private Integer modelYear;
 
     public void setModelYear(Integer modelYear) {
         this.modelYear = modelYear;
+    }
+
+    public Brand getBrand() {
+        return brand;
+    }
+
+    public void setBrand(Brand brand) {
+        this.brand = brand;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
     }
 }
