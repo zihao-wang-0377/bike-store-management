11dabd18500e0cb3c1ff0fa13797c129d3f80a2d
Zijian Ying <z.ying@ostfalia.de>
2024-04-14 00:41:12 +0200

    korrktur bei entity stocks

--- src/main/java/de/pdbm/starter/business/messages/entity/Stock.java
+++ src/main/java/de/pdbm/starter/business/messages/entity/Stock.java
@@ -1,41 +1,48 @@
 package de.pdbm.starter.business.messages.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "stocks")
 public class Stock {
-    @Id
-    private Integer product_id;
-    @Id
-    private Integer store_id;
+    @ManyToOne
+    @MapsId("product_id")
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+    @ManyToOne
+    @MapsId("store_id")
+    @JoinColumn(name = "store_id")
+    private Store store;
+    @EmbeddedId
+    private StockPk stockPk;
     private Integer quantity;
 
     public Stock() {
     }
 
-    public Stock(Integer product_id, Integer store_id, Integer quantity) {
-        this.product_id = product_id;
-        this.store_id = store_id;
-        this.quantity = quantity;
+    public Product getProduct() {
+        return product;
     }
 
-    public Integer getProduct_id() {
-        return product_id;
+    public void setProduct(Product product) {
+        this.product = product;
     }
 
-    public void setProduct_id(Integer product_id) {
-        this.product_id = product_id;
+    public Store getStore() {
+        return store;
     }
 
-    public Integer getStore_id() {
-        return store_id;
+    public void setStore(Store store) {
+        this.store = store;
     }
 
-    public void setStore_id(Integer store_id) {
-        this.store_id = store_id;
+    public StockPk getStockPk() {
+        return stockPk;
+    }
+
+    public void setStockPk(StockPk stockPk) {
+        this.stockPk = stockPk;
     }
 
     public Integer getQuantity() {
---
+++ src/main/java/de/pdbm/starter/business/messages/entity/StockPk.java
@@ -0,0 +1,34 @@
+package de.pdbm.starter.business.messages.entity;
+
+import jakarta.persistence.Embeddable;
+
+import java.io.Serializable;
+@Embeddable
+public class StockPk implements Serializable {
+    private Integer product_id;
+    private Integer store_id;
+
+    public StockPk() {
+    }
+
+    public StockPk(Integer product_id, Integer store_id) {
+        this.product_id = product_id;
+        this.store_id = store_id;
+    }
+
+    public Integer getProduct_id() {
+        return product_id;
+    }
+
+    public void setProduct_id(Integer product_id) {
+        this.product_id = product_id;
+    }
+
+    public Integer getStore_id() {
+        return store_id;
+    }
+
+    public void setStore_id(Integer store_id) {
+        this.store_id = store_id;
+    }
+}
