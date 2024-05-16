b0e1b1972365ed648172a7e0f7fa89162828e69a
Zijian Ying <z.ying@ostfalia.de>
2024-05-16 08:51:52 +0200

    StaffController und service erstellt

--- src/main/java/de/pdbm/starter/business/messages/boundary/control/StaffController.java
+++ src/main/java/de/pdbm/starter/business/messages/boundary/control/StaffController.java
@@ -1,13 +1,26 @@
 package de.pdbm.starter.business.messages.boundary.control;
 
+import de.pdbm.starter.business.messages.entity.Staff;
 import jakarta.enterprise.context.SessionScoped;
 import jakarta.inject.Named;
 
 import java.io.Serializable;
+import java.util.List;
 
 @Named
 @SessionScoped
 public class StaffController implements Serializable {
+    private Integer staffId;
+    private Integer active;
+    private String email;
+    private String firstName;
+    private String nachName;
+    private String phone;
+
+    private String managerId;
+    private Integer storeId;
+
+    private List<Staff> staffList;
     private int clicks;
     public boolean isButtonDisplayed(){
         return clicks % 2 == 1;
--- src/main/java/de/pdbm/starter/business/messages/service/StaffService.java
+++ src/main/java/de/pdbm/starter/business/messages/service/StaffService.java
@@ -3,8 +3,10 @@ package de.pdbm.starter.business.messages.service;
 import de.pdbm.starter.business.messages.entity.Staff;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.TypedQuery;
 
 import java.io.Serializable;
+import java.util.List;
 
 import static jakarta.persistence.PersistenceContextType.TRANSACTION;
 
@@ -15,4 +17,25 @@ public class StaffService implements Serializable {
     public Staff findStaffById(Integer id){
         return  em.find(Staff.class,id);
     }
+
+    public List<Staff> getAllStaff(){
+        TypedQuery<Staff> query = em.createQuery(
+                "select s from Staff s", Staff.class
+        );
+        return query.getResultList();
+    }
+
+    public void save(Staff staff){
+        em.persist(staff);
+    }
+
+    public void delete(Staff staff) {
+        Staff mergedStaff = em.merge(staff);
+        em.remove(mergedStaff);
+    }
+
+    public void update(Staff staff) {
+        em.merge(staff);
+    }
+
 }
