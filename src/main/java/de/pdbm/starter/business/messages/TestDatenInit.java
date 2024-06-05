07fd28bf98a41c5d30ddd9ce83686069fd703dec
Zijian Ying <z.ying@ostfalia.de>
2024-06-05 23:28:47 +0200

    bugs bei Testdata Init beheben

--- src/main/java/de/pdbm/starter/business/messages/TestDatenInit.java
+++ src/main/java/de/pdbm/starter/business/messages/TestDatenInit.java
@@ -1,51 +1,38 @@
 package de.pdbm.starter.business.messages;
 
-import de.pdbm.starter.business.messages.boundary.control.LocalDateConverter;
 import de.pdbm.starter.business.messages.entity.*;
 import de.pdbm.starter.business.messages.service.CustomerService;
-import jakarta.annotation.Priority;
+import jakarta.annotation.PostConstruct;
 import jakarta.ejb.Singleton;
 import jakarta.ejb.Startup;
-import jakarta.annotation.PostConstruct;
 import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
 
 import java.math.BigDecimal;
-import java.time.LocalDate;
-import java.util.Optional;
-
 
 @Startup
 @Singleton
-
 public class TestDatenInit {
-    @PersistenceContext
-    EntityManager em;
-    @Inject
-    CustomerService customerService;
-    @Inject
 
-//    @PostConstruct
-//    public void init() {
-//        em.persist(new Customer( "Wolfenbüttel", "max.schuhmacher@ostfalia.de", "Max", "Schuhmacher", "135-06796623", "TH", "Straße 5", "38300"));
-//    em.persist(new Staff( 1, "admin.staff@bikes.shop", "Admin", "Staff", "(187) 444-3049", null, 1));
-//    }
+    @PersistenceContext(unitName = "test")
+    private EntityManager emDev;
+
+//    @PersistenceContext(unitName = "development")
+//    private EntityManager emProd;
+
+    @Inject
+    private CustomerService customerService;
 
     @PostConstruct
     @Transactional
     public void init() {
-        boolean loadTestData = shouldLoadTestData();
-        if (loadTestData) {
+        if (emDev != null) {
             loadTestData();
         } else {
             System.out.println("Skipping test data initialization in production environment");
         }
-    }
-    private boolean shouldLoadTestData() {
-        String loadTestDataProperty = System.getProperty("loadTestData");
-        return Boolean.parseBoolean(Optional.ofNullable(loadTestDataProperty).orElse("false"));
     }
 
     private void loadTestData() {
@@ -56,54 +43,52 @@ public class TestDatenInit {
         insertStore();
         insertOrder();
     }
+
     private void insertStaffs() {
-        // Laden Sie das Store-Objekt
-        Store store = em.find(Store.class, 1);
+        Store store = emDev.find(Store.class, 1);
 
-        // Erstellen Sie das Staff-Objekt ohne Manager
         Staff adminStaff = new Staff(1, "admin.staff@bikes.shop", "Admin", "Staff", "(187) 444-3049", null, store);
-        em.persist(adminStaff);
+        emDev.persist(adminStaff);
 
-        // Fügen Sie weitere Staff-Objekte hinzu, ggf. mit Manager
         Staff user1Staff = new Staff(1, "user1.staff@bikes.shop", "User1", "Staff", "(187) 322-3224", adminStaff, store);
-        em.persist(user1Staff);
+        emDev.persist(user1Staff);
 
         Staff user2Staff = new Staff(1, "user2.staff@bikes.shop", "User2", "Staff", "(187) 333-7543", user1Staff, store);
-        em.persist(user2Staff);
+        emDev.persist(user2Staff);
 
         Staff test1Staff = new Staff(1, "test1.staff@ostfalia.de", "Test1", "Staff", "(187) 114-0000", user1Staff, store);
-        em.persist(test1Staff);
+        emDev.persist(test1Staff);
 
         Staff test2Staff = new Staff(1, "test2.staff2@ostfalia.de", "Test2", "Staff", "(187) 114-0404", user2Staff, store);
-        em.persist(test2Staff);
+        emDev.persist(test2Staff);
     }
 
-    private void insertCustomer(){
+    private void insertCustomer() {
         Customer test1Customer = new Customer("Wolfenbuttel", "1234@gmail.com", "ma", "Elon", "(559) 628-2239", "HH", "12 exer", "38302");
-        em.persist(test1Customer);
+        emDev.persist(test1Customer);
         Customer test2Customer = new Customer("Wolfenbuttel", "12344@gmail.com", "mask", "michel", "(559) 628-2239", "HH", "12 exer", "38304");
-        em.persist(test2Customer);
+        emDev.persist(test2Customer);
     }
 
-    private void insertBrand(){
+    private void insertBrand() {
         Brand test1Brand = new Brand("Big bike");
-        em.persist(test1Brand);
+        emDev.persist(test1Brand);
         Brand test2Brand = new Brand("1");
-        em.persist(test2Brand);
+        emDev.persist(test2Brand);
     }
-    private void insertStore(){
+
+    private void insertStore() {
         Store test1Store = new Store();
-        em.persist(test1Store);
+        emDev.persist(test1Store);
     }
-    private void insertProduct(){
+
+    private void insertProduct() {
         Product product = new Product(new BigDecimal(100), 2024, "autobike", null, null);
-        em.persist(product);
+        emDev.persist(product);
     }
 
-    private void insertOrder(){
+    private void insertOrder() {
         Order order = new Order();
-        em.persist(order);
+        emDev.persist(order);
     }
-
-
 }
